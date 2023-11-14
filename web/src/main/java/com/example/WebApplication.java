9dab4ee3548d1740b54a29af8beae9ed1178a8a9
Javier <jmartin@grupoloyal.com>
2023-11-14 11:36:18 +0100

    Repository

--- web/src/main/java/com/example/WebApplication.java
+++ web/src/main/java/com/example/WebApplication.java
@@ -10,6 +10,8 @@ import com.example.IoC.EjemplosIoC;
 import com.example.IoC.Inyectable;
 import com.example.IoC.OtroComponente;
 import com.example.IoC.Tonteria;
+import com.example.domains.contracts.reposiries.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class WebApplication implements CommandLineRunner {
@@ -18,32 +20,50 @@ public class WebApplication implements CommandLineRunner {
 		SpringApplication.run(WebApplication.class, args);
 	}
 
-	@Autowired
-//	@Qualifier("antigua")
-	Inyectable srv;
-
-	@Autowired(required = false)
-	OtroComponente comp;
+//	@Autowired
+////	@Qualifier("antigua")
+//	Inyectable srv;
+//
+//	@Autowired(required = false)
+//	OtroComponente comp;
+//	
+//	@Autowired
+//	Tonteria unaTonteria;
+//	
+//	@Autowired
+//	Tonteria unaTonteriaDep;
+//	
+//	@Autowired
+//	EjemplosIoC eje;
 	
 	@Autowired
-	Tonteria unaTonteria;
-	
-	@Autowired
-	Tonteria unaTonteriaDep;
-	
-	@Autowired
-	EjemplosIoC eje;
+	ActorRepository dao;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicacion arrancada");
-		srv.run();
-		srv.add();
-		srv.cuantos();
-		if(comp != null) comp.exec();
-		unaTonteria.dime();
-		unaTonteriaDep.dime();
-		System.out.println(eje.toString());
+		
+//		dao.save(new Actor(0, "Pepito", "Grillo"));
+//		var item = dao.findById(201);
+//		if(item.isPresent()) {
+//			Actor actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//		}
+//		dao.deleteById(201);
+		dao.findAll().forEach(System.out::println);
+		
+	}
+	
+	public void runIoC(String... args) throws Exception {
+		System.out.println("Aplicacion arrancada");
+//		srv.run();
+//		srv.add();
+//		srv.cuantos();
+//		if(comp != null) comp.exec();
+//		unaTonteria.dime();
+//		unaTonteriaDep.dime();
+//		System.out.println(eje.toString());
 		
 //		var x = new EjemplosIoC(1, "Javi");
 //		System.out.println(x.toString());
